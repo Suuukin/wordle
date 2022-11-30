8572a1f1f2b5d73de2b2cf49e463c37f1d551156
Suuukin <e.c.schemenauer@gmail.com>
2022-11-30 13:49:33 -0800

    Fixed rows and added wordlist.

---
+++ simple_wordle.py
@@ -0,0 +1,43 @@
+import random
+import wordlist
+
+wordle = random.choice(wordlist.wordle_list)
+letters = list(wordle)
+ask_end_game = True
+
+print(wordle)
+
+position_hints = input("Do you want positional hints (y, n, detailed)? ")
+
+def position_check(string, guess):
+    positions = [] #list to store positions for each 'char' in 'wordle'
+    for location in range(len(string)):
+        if string[location] == guess:
+            positions.append(location+1)
+    return positions
+
+
+while True:
+    guess = input("Guess a letter in the wordle. ")
+
+    if guess in list(wordle):
+        print(f"The letter {guess} is in the wordle.")
+
+        if position_hints == "y":
+            position = wordle.find(guess)
+            print(f"The first position of your guess in the wordle is {position+1}. ")
+        elif position_hints == "detailed":
+            positions = position_check(wordle, guess)
+            print(f"Your guess is in the position(s) {positions}. ")
+    else:
+        print(f"The letter {guess} is not in the wordle.")
+
+    if ask_end_game:
+        end_game = input("Want to end the game (y,n)? To turn off this question type 'never'. ")
+        if end_game == "never":
+            ask_end_game = False
+
+    if end_game == "y":
+        break
+
+print("Thanks for playing!")
--- wordle.py
+++ wordle.py
@@ -1,36 +1,70 @@
 import tkinter as tk
 import tkinter.font as font
 from PIL import ImageTk, Image
+import wordlist
+import random
+# flake8: noqa
 
 
 class State:
     labels = {}  # {point, label}
+    buttons = {} # {key_text, button}
+    wordle = random.choice(wordlist.wordle_list)
+    keyboard_frames = {}
 
+print(State.wordle)
 
 def label_maker(frame):
     return tk.Label(frame, text="x", font=GRID_FONT, borderwidth=3, padx=20, pady=10)
 
+def btn_maker(frame, text):
+    return tk.Button(frame, text=text, font=KEYBOARD_FONT, operation=btn_op(text))
+
+def btn_op(text):
+    return
+
+KEY_ROWS = [
+    "qwertyuiop",
+    "asdfghjkl",
+    "zxcvbnm"
+]
 
 def main():
     global GRID_FONT
+    global KEYBOARD_FONT
+
     window = tk.Tk()
     window.title("Wordle")
     window.resizable(False, False)
-    GRID_FONT = font.Font(family="Courier", size=50, weight="bold")
 
-    guess_area = tk.Frame(window, width=300, height=50, bg="honeydew2")
-    keyboard_row_1 = tk.Frame(window, width=300, height=50, bg="honeydew2")
-    keys_row_1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
-    keyboard_row_2 = tk.Frame(window, width=300, height=50, bg="honeydew2")
-    keys_row_2 = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
-    keyboard_row_3 = tk.Frame(window, width=300, height=50, bg="honeydew2")
-    for y in range(1, 6):
-        for x in range(1, 5):
+    GRID_FONT = font.Font(family="Courier", size=50, weight="bold")
+    KEYBOARD_FONT = font.Font(family='Courier', size=30, weight="bold")
+
+    guess_area = tk.Frame(window, width=300, height=200, bg="honeydew2")
+    guess_area.grid(row=0, column=1)
+
+
+    for y in range(1, 7):
+        for x in range(1, 6):
             label = label_maker(guess_area)
             State.labels[(x, y)] = label
-            label.grid(row=x, column=y, sticky="nsew")
+            label.grid(row=y, column=x, sticky="nsew")
 
-    guess_area.grid(row=1, column=1, sticky="nsew")
+
+    for row, key_row in enumerate(KEY_ROWS):
+        State.keyboard_frames[row] = frame = tk.Frame(window, width=300, height=50, bg="honeydew2")
+        frame.grid(row=row+2, column=1)
+        keys = list(key_row)
+        for column, key_text in enumerate(keys):
+            State.buttons[key_text] = button = btn_maker(frame, key_text)
+            button.grid(row=1, column=column+1)
+
+    enter_btn = btn_maker(State.keyboard_frames[2], "ENTER")
+    enter_btn.grid(row=1, column=0)
+    clear_btn = btn_maker(State.keyboard_frames[2], "CE")
+    clear_btn.grid(row=1, column=10)
+
+
     window.mainloop()
 
 
