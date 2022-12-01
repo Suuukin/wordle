00e9c69449c6661ca6af42ca3a56b46c0df0e05c
Eric Schemenauer <e.c.schemenauer@gmail.com>
2022-12-01 11:43:30 -0800

    Fixed menu for wordle and created simple_wordle.

--- simple_wordle.py
+++ simple_wordle.py
@@ -1,99 +1,46 @@
 import random
 import wordlist
 
-# flake8: noqa
+wordle = random.choice(wordlist.wordle_list)
+letters = list(wordle)
+ask_end_game = True
 
-class State:
-    """Class to hold global variables."""
-    wordle = random.choice(wordlist.wordle_list)
-    letters = list(wordle)
-    ask_end_game = True
-    guess_words = None
-    position_hints = None
-    invalid_guess = False
+print(wordle)
+
+position_hints = input("Do you want positional hints (y, n, detailed)? ")
 
 
-State.guess_words = input("Do you want to enter full words as guesses (y,n)? ")
-State.position_hints = input("Do you want positional hints (y, n, detailed, colors)? ")
- 
-print(State.wordle)
-
-
-def detailed_position_check(guess_letter):
-    """Checks for what positions the letter is in the wordle.
-    Then returns all the positions that the letter is in."""
+def position_check(string, guess):
     positions = []  # list to store positions for each 'char' in 'wordle'
-    for i, location in enumerate(State.letters):
-        if State.letters[i] == guess_letter:
-            positions.append(i + 1)
+    for location in range(len(string)):
+        if string[location] == guess:
+            positions.append(location + 1)
     return positions
 
 
-def position_check(letter, guess_word=None, i=None):
-    """Selects which hint and positional clues the simple_wordle should return."""
-    if State.position_hints == "colors":
-        if letter not in State.letters: 
-            # if the letter not in the word then the color is red
-            color = "Red"
-        else:
-            # sets the color to yellow if in the word
-            color = "Yellow"
-        if State.wordle[i] == letter:
-            color = "Green"
-        print(f"{letter} is {color}.")
-        return
-
-    if letter in State.letters:
-        if State.position_hints == "y":
-            # if position_hints is simple mode than it
-            # returns the first position of the guessed letter in the word
-            position = State.wordle.find(letter)
-            print(f"The first position of {letter} in the wordle is {position+1}. ")
-
-        elif State.position_hints == "detailed":
-            # if position_hints is complex than it returns all positions of the guessed letter
-            positions = detailed_position_check(letter)
-            print(f"Letter {letter} is in the position(s) {positions}. ")
-
-    else:
-        print(f"The letter {letter} is not in the wordle.")
-
-
-def word_in_wordle_list():
-    if guess_word not in wordlist.wordle_list:
-        State.invalid_guess = True
-        print("Your guessed word is not in the wordle list.")
-    else:
-        State.invalid_guess = False
-    
-
 while True:
-    """Loop that runs the game until the wordle is guessed."""
-    if State.guess_words == "y":
-        guess_word = input("Guess a five letter word. ")
-        word_in_wordle_list()
-        if not State.invalid_guess:
-            if guess_word == State.wordle:
-                # ends loop if guessed word is correct
-                print("You guessed the Wordle!")
-                break
-            else:
-                for i, letter in enumerate(guess_word):
-                    # checks all the letters and returns the correct positional hints
-                    position_check(letter, guess_word, i)
+    guess = input("Guess a letter in the wordle. ")
 
+    if guess in list(wordle):
+        print(f"The letter {guess} is in the wordle.")
+
+        if position_hints == "y":
+            position = wordle.find(guess)
+            print(f"The first position of your guess in the wordle is {position+1}. ")
+        elif position_hints == "detailed":
+            positions = position_check(wordle, guess)
+            print(f"Your guess is in the position(s) {positions}. ")
     else:
-        guess_letter = input("Guess a letter in the wordle. ")
-        position_check(guess_letter)
+        print(f"The letter {guess} is not in the wordle.")
 
-#    if State.ask_end_game:
-#        end_game = input(
-#            "Want to end the game (y,n)? To turn off this question type 'never'. "
-#        )
-#        if end_game == "never":
-#            ask_end_game = False
-#
-#    if end_game == "y":
-#        break
+    if ask_end_game:
+        end_game = input(
+            "Want to end the game (y,n)? To turn off this question type 'never'. "
+        )
+        if end_game == "never":
+            ask_end_game = False
+
+    if end_game == "y":
+        break
 
 print("Thanks for playing!")
--- wordle.py
+++ wordle.py
@@ -1,18 +1,27 @@
 import tkinter as tk
 import tkinter.font as font
 from PIL import ImageTk, Image
-
+import wordlist
+import random
 # flake8: noqa
+
+
 class State:
     labels = {}  # {point, label}
     buttons = {} # {key_text, button}
+    wordle = random.choice(wordlist.wordle_list)
+    keyboard_frames = {}
 
+print(State.wordle)
 
 def label_maker(frame):
     return tk.Label(frame, text="x", font=GRID_FONT, borderwidth=3, padx=20, pady=10)
 
 def btn_maker(frame, text):
-    return tk.Button(frame, text=text, font=KEYBOARD_FONT)
+    return tk.Button(frame, text=text, font=KEYBOARD_FONT, operation=btn_op(text))
+
+def btn_op(text):
+    return
 
 KEY_ROWS = [
     "qwertyuiop",
@@ -31,23 +40,30 @@ def main():
     GRID_FONT = font.Font(family="Courier", size=50, weight="bold")
     KEYBOARD_FONT = font.Font(family='Courier', size=30, weight="bold")
 
-    guess_area = tk.Frame(window, width=300, height=50, bg="honeydew2")
-    guess_area.grid(row=1, column=1)
+    guess_area = tk.Frame(window, width=300, height=200, bg="honeydew2")
+    guess_area.grid(row=0, column=1)
+
+
+    for y in range(1, 7):
+        for x in range(1, 6):
+            label = label_maker(guess_area)
+            State.labels[(x, y)] = label
+            label.grid(row=y, column=x, sticky="nsew")
+
 
     for row, key_row in enumerate(KEY_ROWS):
-        frame = tk.Frame(window, width=300, height=50, bg="honeydew2")
+        State.keyboard_frames[row] = frame = tk.Frame(window, width=300, height=50, bg="honeydew2")
         frame.grid(row=row+2, column=1)
         keys = list(key_row)
         for column, key_text in enumerate(keys):
             State.buttons[key_text] = button = btn_maker(frame, key_text)
-            button.grid(row=1, column=column)
-            print(frame, column)
+            button.grid(row=1, column=column+1)
 
-    for y in range(1, 6):
-        for x in range(1, 5):
-            label = label_maker(guess_area)
-            State.labels[(x, y)] = label
-            label.grid(row=x, column=y, sticky="nsew")
+    enter_btn = btn_maker(State.keyboard_frames[2], "ENTER")
+    enter_btn.grid(row=1, column=0)
+    clear_btn = btn_maker(State.keyboard_frames[2], "CE")
+    clear_btn.grid(row=1, column=10)
+
 
     window.mainloop()
 
